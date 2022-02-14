79843206f85fba20dfa0a6be25c073167e7dc56d
Andy <awilliamscomposing@gmail.com>
2022-02-13 22:01:21 -0800

    Expanding functionality and accessibility

--- scripts/script.js
+++ scripts/script.js
@@ -29,6 +29,7 @@ const checkText = () => {
   const userText = document.getElementById("userText").value;
   // const countLetters = userText.replace(/\s+/g, "").length;
   // const finalCount = `Your text contains:`;
+  // const characterTotal = document.getElementById("characterTotal");
 
   if (userText === "") {
     console.log("Please enter some text.");
@@ -85,6 +86,31 @@ const checkText = () => {
   // const finalCount = `Your text contains: \n ${countLetters} letters. ${countNumbers} numbers. ${countCharacters} characters. ${countSpecialCharacters} special characters. ${countWords} words. ${countSentences} sentences.`;
   // document.getElementById("finalCount").innerHTML = finalCount;
   // const finalCount = `Your text contains:`;
+
+  // characterTotal.innerHTML = userText.length + " characters";
+
+  counterFunction();
+};
+
+const counterFunction = () => {
+  const characterLimit = document.getElementById("characterLimit");
+  const characterTotal = document.getElementById("characterTotal");
+  const userText = document.getElementById("userText").value;
+
+  const maxLength = 100;
+  const currentLength = userText.length;
+  const remainingCount = maxLength - currentLength;
+
+  if (remainingCount < 0) {
+    characterLimit.innerHTML =
+      '<span style="color: red;">You have exceeded the limit of ' +
+      maxLength +
+      " characters</span>";
+  } else {
+    characterLimit.innerHTML = remainingCount + " characters remaining";
+  }
+
+  characterTotal.innerHTML = userText.length + " characters";
 };
 
 // ----------------------------------------------------------------
